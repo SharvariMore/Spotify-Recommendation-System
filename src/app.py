a9870493a83e7baef24d0948532337bcbfa28823
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2024-06-03 12:20:55 -0400

    Add files via upload

---
+++ src/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, render_template, send_from_directory
+
+from flask_cors import CORS
+import json
+import pandas as pd
+from utils import DatabaseCaller
+
+
+def load_config(config_path):
+    """Load the ETL configuration file."""
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    return config
+
+base_dir = '../'
+# config = load_config(base_dir + 'configs/etl_config.json')
+config_db = load_config(base_dir + 'db/db_config.json')
+
+dBConfig = config_db["spotify_db"]
+caller = DatabaseCaller(dBConfig)
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/",methods=['GET', 'POST'])
+@app.route("/home",methods=['GET', 'POST'])
+def home():
+    songs = caller.getSongs()
+    songs = songs['Track Name'].unique().tolist()
+    return render_template("search.html",songs = songs)
+
+
+# @app.route("/recommend")
+# def dashboard():
+#     return render_template("dashboard.html")
+
+@app.route("/results",methods=['POST'])
+def results():
+    '''
+    Endpoint that handles the search functionality and displays results.
+    '''
+    if request.method == 'POST':
+        searchTerm = request.form.get('search-term')
+        #db search logic below
+        results = ['test']
+        return render_template('dashboard.html', results=results)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002,threaded=False)
+    
+
+
